109094bbe1992ba53cff3922e232998b1a5e9f19
Jdsoul <88025603+Jdsoul@users.noreply.github.com>
2021-08-08 13:17:21 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/mwcinema"
                                 )
                         ]
                     ]
@@ -47,8 +47,8 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/starkindusttri'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://telegra.ph/file/bf1a8b7cb18a95892a50c.jpg')
     ],[
         InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
     ],[
